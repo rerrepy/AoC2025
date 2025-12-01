6d74c4deaa7c3a032e262cfcef9051910377ce40
Rikard Carlsson <rikard.carlsson91@gmail.com>
2025-12-01 18:10:18 +0100

    Initial

---
+++ day1_part1.py
@@ -0,0 +1,12 @@
+dial=50
+code=0
+with open ('day1.txt', 'r') as file:
+    for line in file:
+        rotation=int(line.strip()[1:])
+        if line[0] == 'L':
+            dial -= rotation
+        else:
+            dial += rotation
+        if dial % 100 == 0:
+            code += 1
+print(code)
